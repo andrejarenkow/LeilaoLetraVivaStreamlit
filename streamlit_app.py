fbc805ee485637f96e09a60afe872244aa14d2a1
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2023-12-09 08:31:48 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -232,18 +232,18 @@ with col3:
                  })
  
 total_historico_valores = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQWwT_7xvVyE_Yu1UeBfBKm8eq-biwQ0toD94DFAwPA0cvX-HBq6SajnyEIJRkujHiQTEiiHR_Q34kq/pub?gid=0&single=true&output=csv')
-historico_limpo= total_historico_valores.drop_duplicates(subset=['peca'], keep='first')
+historico_limpo= total_historico_valores.drop_duplicates(subset=['peca', 'valor'], keep='first')
 historico_limpo = pd.pivot_table(historico_limpo, index='data', values='valor', aggfunc='sum').reset_index()
 historico_limpo['somatorio'] = historico_limpo['valor'].cumsum()
 historico_limpo['data'] = pd.to_datetime(historico_limpo['data']).dt.strftime('%Y-%m-%d')
 
 try:
  ontem = (datetime.datetime.today() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
- valor_vendido_ontem = historico_limpo[historico_limpo['data']==ontem]['somatorio'].values[0]
+ valor_vendido_ontem = historico_limpo[historico_limpo['data']==ontem]['valor'].values[0]
 
 except:
  ontem = (datetime.datetime.today() - datetime.timedelta(days=2)).strftime('%Y-%m-%d')
- valor_vendido_ontem = historico_limpo[historico_limpo['data']==ontem]['somatorio'].values[0]
+ valor_vendido_ontem = historico_limpo[historico_limpo['data']==ontem]['valor'].values[0]
  
 with col1:
    #st.metric('Potencial', f'R$ {dados["preço"].sum():,.2f}')
@@ -254,20 +254,20 @@ with col1:
  
    st.metric('Total de Visitas', dados['visitas'].sum())
  
-   st.metric('Total de LAnces', dados['lances'].sum())
+   st.metric('Total de Lances', dados['lances'].sum())
  
    st.metric('Itens com lances', f"{(dados['lancado'].sum()/len(dados['lancado'])*100).round(1)} %")
  
  
  
 with col2:
-   fig = px.line(historico_limpo, x='data', y='somatorio', title="Histórico do valor total de vendas", markers=True)
+   fig = px.line(historico_limpo, x='data', y='valor', title="Histórico do valor total de vendas", markers=True)
    # Set x-axis title
    fig.update_xaxes(title_text="Data")
    
    # Set y-axes titles
    fig.update_yaxes(title_text="Venda total")
-   #st.plotly_chart(fig, use_container_width=True)
+   st.plotly_chart(fig, use_container_width=True)
 
 
 
